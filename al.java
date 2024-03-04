e781bb31988052166e54efabb1a5722c79decc86
Mao Xingyu <160716263+Mao-Xingyu@users.noreply.github.com>
2024-03-04 17:16:51 +0800

    Update al.java

--- al.java
+++ al.java
@@ -7,19 +7,29 @@ public class al {
         ArrayList = a resizable array.
                     Elements can be added and removed after compilation phase           
                     store reference data types
-         */
+        */
 
-         ArrayList<String> food = new ArrayList<String>();
+        ArrayList<String> food = new ArrayList<String>();
 
-         food.add("pizza");
-         food.add("hamburger");
-         food.add("hotdog");
-         food.add("fried rice");
+        //add function adds element into the next unused index. (0-3)
+        food.add("pizza");
+        food.add("hamburger");
+        food.add("hotdog");
+        food.add("fried rice");
+        
+        //set function adds element to specific index, allowing overwrite
+        food.set(5, "horfan");
 
-         for(int i=0; i<food.size(); i++)
-         {
+        //remove function removes the element of said index
+        food.remove(5);
+
+        //clear function all elements from the arraylist
+        food.clear();
+
+        for(int i=0; i<food.size(); i++)
+        {
             System.out.println(food.get(i));
-         }
+        }
 
     }
 }
